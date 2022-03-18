8649c9e30698fd44a058c90af1cf8fef67df27d1
lemmenri <rick@joberi.nl>
2022-03-18 13:12:32 +0100

    added footer content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,19 @@
 import React from 'react'
 
 export default function Footer() {
-  return (
-    <footer className='p-4 text-white bg-gray-700'>Footer</footer>
-  )
+    return (
+        <footer className='p-4 text-gray-300 bg-gray-700'>
+            <p className='m-8 text-sm'>
+                Portions of MTG-Library are unofficial Fan Content permitted under the Wizards of the Coast Fan Content Policy. The literal and graphical information presented on this site about Magic: The Gathering, including card images, the mana symbols, and Oracle text, is copyright Wizards of the Coast, LLC, a subsidiary of Hasbro, Inc. MTG-Library is not produced by, endorsed by, supported by, or affiliated with Wizards of the Coast.
+                <br />
+                <br />
+                Card images and card data by Scryfall
+                <br />
+                Icons by Heroicons
+                <br />
+                <br />
+                All other content © {new Date().getFullYear()} MTG-Library.
+            </p>
+        </footer>
+    )
 }
