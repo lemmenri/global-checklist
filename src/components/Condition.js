8336c3708ae79b74a4d8b84c7a9b8a0e7d9fde10
lemmenri <rick@joberi.nl>
2022-05-17 11:34:30 +0200

    Expanded Condition component tests

--- src/components/Condition.js
+++ src/components/Condition.js
@@ -10,3 +10,7 @@ export default function Condition({ condition }) {
     </p>
   );
 }
+
+Condition.defaultProps = {
+  condition: "NM",
+};
--- src/components/Condition.spec.js
+++ src/components/Condition.spec.js
@@ -3,7 +3,26 @@ import "tailwindcss/tailwind.css";
 import { mount } from "@cypress/react";
 import Condition from "./Condition";
 
-it("External Link", () => {
+it("Condition MT", () => {
+  mount(<Condition condition={"MT"} />);
+  cy.get("p").contains("MT").should("have.class", "bg-MT");
+  cy.get("p").should("have.css", "background-color", "rgb(23, 162, 184)");
+});
+
+it("Condition NM", () => {
   mount(<Condition condition={"nm"} />);
   cy.get("p").contains("NM").should("have.class", "bg-NM");
+  cy.get("p").should("have.css", "background-color", "rgb(61, 176, 87)");
+});
+
+it("Invalid Condition", () => {
+  mount(<Condition condition={"hoi"} />);
+  cy.get("p").contains("HOI").should("have.class", "bg-HOI");
+  cy.get("p").should("have.css", "background-color", "rgba(0, 0, 0, 0)");
+});
+
+it("No Condition Provided / Default Condition", () => {
+  mount(<Condition />);
+  cy.get("p").contains("NM").should("have.class", "bg-NM");
+  cy.get("p").should("have.css", "background-color", "rgb(61, 176, 87)");
 });
