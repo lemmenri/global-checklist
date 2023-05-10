c09adbb26a68e938f74a63c4c085dd7d25abb239
lemmenri <rick@joberi.nl>
2023-05-10 15:55:22 +0200

    updated language component tests

--- src/components/Language.cy.js
+++ src/components/Language.cy.js
@@ -3,8 +3,30 @@ import "tailwindcss/tailwind.css";
 import "../App.css";
 import Language from "./Language";
 
-it("Language", () => {
+it("displays language label", () => {
   cy.mount(<Language language="en" />);
-  cy.get("p").should("have.text", "EN");
-  cy.get("p").should("have.css", "background-color", "rgb(4, 42, 51)");
+  cy.get('#language-label-en').should("have.text", "EN");
+  cy.get('#language-label-en').should("have.css", "background-color", "rgb(4, 42, 51)");
 });
+
+it('displays image on hover', () => {
+  cy.mount(<Language language={"zhs"} imageOnHover={"https://cards.scryfall.io/normal/front/6/a/6af5e30b-a23c-4b37-8702-fff80982c653.jpg?1562613047"} />);
+  cy.get('#language-label-zhs')
+    .trigger('mouseover')
+  cy.get('img')
+    .should('exist')
+    .and('be.visible')
+})
+
+it('hides image on mouseout', () => {
+  cy.mount(<Language language={"zhs"} imageOnHover={"https://cards.scryfall.io/normal/front/6/a/6af5e30b-a23c-4b37-8702-fff80982c653.jpg?1562613047"} />);
+  cy.get('#language-label-zhs')
+    .trigger('mouseover')
+  cy.get('img')
+    .should('exist')
+    .and('be.visible')
+  cy.get('#language-label-zhs')
+    .trigger('mouseout', { force: true })
+  cy.get('img')
+    .should('not.be.visible')
+})
