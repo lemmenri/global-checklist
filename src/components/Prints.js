c4544e413a8194bb2ee7635fed3f9124bca93d9b
lemmenri <rick@joberi.nl>
2024-04-09 08:28:28 +0200

    refactored prints component

--- src/components/Prints.js
+++ src/components/Prints.js
@@ -1,15 +1,7 @@
 import React from "react";
-import { useNavigate } from "react-router";
 import PrintsListItem from "./PrintsListItem";
 
-export default function Prints({ prints, cardname, setCardId, cardId }) {
-    const navigate = useNavigate();
-    const goToAllPrints = () => {
-        navigate({
-            pathname: "/search",
-            search: `?name=${cardname}`,
-        });
-    };
+export default function Prints({ prints, setCardId, cardId }) {
 
     return (
         <div id="prints-list" className="py-4">
@@ -19,25 +11,19 @@ export default function Prints({ prints, cardname, setCardId, cardId }) {
             >
                 Prints
             </div>
-            {prints.slice(0, 10).map((print) => (
-                <PrintsListItem
-                    key={`${print.setName}-${print.collector_number}`}
-                    collected={print.collected}
-                    setName={print.setName}
-                    collector_number={print.collector_number}
-                    id={print.id}
-                    image={print.image}
-                    setCardId={setCardId}
-                    bold={print.id === cardId}
-                />
-            ))}
-            {prints.length > 10 && (
-                <div id="all-prints" className="py-1 px-2 border border-t-0 border-primary">
-                    <button onClick={goToAllPrints} className="hover:underline" id="cardName">
-                        View all prints...
-                    </button>
-                </div>
-            )}
+            <div className=" max-h-72 overflow-y-auto overflow-x-hidden">
+                {prints.map((print) => (
+                    <PrintsListItem
+                        key={`${print.setName}-${print.collector_number}`}
+                        collected={print.collected}
+                        setName={print.setName}
+                        collector_number={print.collector_number}
+                        id={print.id}
+                        setCardId={setCardId}
+                        bold={print.id === cardId}
+                    />
+                ))}
+            </div>
             {prints.length === 0 && (
                 <div id="no-other-prints" className="py-1 px-2 border border-primary">
                     No other prints
