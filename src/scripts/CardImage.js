e07ae6357044a4d24ded0704deabe67d788df90e
lemmenri <rick@joberi.nl>
2024-04-09 08:13:08 +0200

    added support for fetching backside of images

--- src/scripts/CardImage.js
+++ src/scripts/CardImage.js
@@ -1,8 +1,22 @@
 // Returns the correct image url from a scryfall card object
 export const getCardImage = (card) => {
-  return card.hasOwnProperty("image_uris") // check if image is available
+  const frontUri = card.hasOwnProperty("image_uris") // check if image is available
     ? card.image_uris.normal
     : card.card_faces[0].hasOwnProperty("image_uris") // check for double faced card
-    ? card.card_faces[0].image_uris.normal
-    : "https://c2.scryfall.com/file/scryfall-errors/soon.jpg";
+      ? card.card_faces[0].image_uris.normal
+      : "https://c2.scryfall.com/file/scryfall-errors/soon.jpg";
+
+  let backUri = undefined
+  const hasBack = !card.hasOwnProperty("image_uris") && card.card_faces[1].hasOwnProperty("image_uris")
+
+  if (hasBack) {
+    backUri = card.card_faces[1].image_uris.normal
+  }
+
+  const faces = {
+    front: frontUri,
+    ...(hasBack) && { back: backUri }
+  }
+
+  return faces
 };
